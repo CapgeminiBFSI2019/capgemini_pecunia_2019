d5e7ef7895cbda92bbf7ee88bf8efe2590428909
Arpan1997 <arpanmondal1902@gmail.com>
2019-09-11 17:00:34 +0530

    branch

---
+++ Pecunia/src/main/java/com/capgemini/pecunia/model/Branch.java
@@ -0,0 +1,50 @@
+package com.capgemini.pecunia.model;
+
+public class Branch 
+{
+	private String branchId;
+	private String branchName;
+	public Branch(String branchId, String branchName, String branchIFSC, String branchAddress) 
+	{
+		super();
+		this.branchId = branchId;
+		this.branchName = branchName;
+		this.branchIFSC = branchIFSC;
+		this.branchAddress = branchAddress;
+	}
+	private String branchIFSC;
+	private String branchAddress;
+	public String getBranchId() 
+	{
+		return branchId;
+	}
+	public void setBranchId(String branchId) 
+	{
+		this.branchId = branchId;
+	}
+	public String getBranchName() 
+	{
+		return branchName;
+	}
+	public void setBranchName(String branchName) 
+	{
+		this.branchName = branchName;
+	}
+	public String getBranchIFSC() 
+	{
+		return branchIFSC;
+	}
+	public void setBranchIFSC(String branchIFSC)
+	{
+		this.branchIFSC = branchIFSC;
+	}
+	public String getBranchAddress() 
+	{
+		return branchAddress;
+	}
+	public void setBranchAddress(String branchAddress) 
+	{
+		this.branchAddress = branchAddress;
+	}
+
+}
