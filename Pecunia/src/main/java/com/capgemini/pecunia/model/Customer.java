cd44c23065cdb5d2ff41b10feaa532ec28f0490c
aninrana <aninrana@DIN80016811.corp.capgemini.com>
2019-09-11 16:17:54 +0530

    Customer Added

---
+++ Pecunia/src/main/java/com/capgemini/pecunia/model/Customer.java
@@ -0,0 +1,69 @@
+package com.capgemini.pecunia.model;
+import java.util.*;
+
+public class Customer {
+private String customerId;
+private String customerName;
+private String customerAadhar;
+private String customerPan;
+private String customerContact;
+private String customerGender;
+private Date customerDob;
+
+
+	public Customer() {
+		
+	}
+
+	public void setCustomerId(String customerId) {
+		this.customerId = customerId;
+	}
+	
+	public String getCustomerId() {
+		return customerId;
+	}
+	
+	public void setCustomerName(String customerName) {
+		this.customerName = customerName;
+	}
+	
+	public String getCustomerName() {
+		return customerName;
+	}
+	public void setCustomerAadhar(String  customerAadhar) {
+		this. customerAadhar =  customerAadhar;
+	}
+	
+	public String getCustomerAadhar() {
+		return customerAadhar;
+	}
+	
+	public void setCustomerPan(String customerPan) {
+		this.customerPan = customerPan;
+	}
+	
+	public String getCustomerPan() {
+		return customerPan;
+	}
+	public void setCustomerContact(String customerContact) {
+		this.customerContact = customerContact;
+	}
+	
+	public String getCustomerContact() {
+		return customerContact;
+	}
+	public void setCustomerGender(String customerGender) {
+		this.customerGender = customerGender;
+	}
+	
+	public String getCustomerGender() {
+		return customerGender;
+	}
+	public void setCustomerDob(Date customerDob) {
+		this.customerDob = customerDob;
+	}
+	
+	public Date getCustomerDob() {
+		return customerDob;
+	}
+}
---
+++ Pecunia/src/main/java/com/capgemini/pecunia/model/package-info.java
@@ -0,0 +1,1 @@
+package com.capgemini.pecunia.model;
