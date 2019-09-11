e1d4f9ed78f88a52af9bf9df737fd20650906706
basuanish <55137730+basuanish@users.noreply.github.com>
2019-09-11 16:44:38 +0530

    Cheque class added

---
+++ Pecunia/src/main/java/com/capgemini/pecunia/model/Cheque.java
@@ -0,0 +1,77 @@
+package com.capgemini.pecunia.model;
+
+import java.util.Date;
+
+public class Cheque {
+	private String chequeId;
+	private int chequeNum;
+	private String chequeAccountNo;
+	private String chequeHolderName;
+	private String chequeBankName;
+	private String chequeIFSC;
+	private Date chequeIssueDate;
+	private String chequeStatus;
+	public Cheque(String chequeId, int chequeNum, String chequeAccountNo, String chequeHolderName,
+			String chequeBankName, String chequeIFSC, Date chequeIssueDate, String chequeStatus) {
+		super();
+		this.chequeId = chequeId;
+		this.chequeNum = chequeNum;
+		this.chequeAccountNo = chequeAccountNo;
+		this.chequeHolderName = chequeHolderName;
+		this.chequeBankName = chequeBankName;
+		this.chequeIFSC = chequeIFSC;
+		this.chequeIssueDate = chequeIssueDate;
+		this.chequeStatus = chequeStatus;
+	}
+	public String getChequeId() {
+		return chequeId;
+	}
+	public void setChequeId(String chequeId) {
+		this.chequeId = chequeId;
+	}
+	public int getChequeNum() {
+		return chequeNum;
+	}
+	public void setChequeNum(int chequeNum) {
+		this.chequeNum = chequeNum;
+	}
+	public String getChequeAccountNo() {
+		return chequeAccountNo;
+	}
+	public void setChequeAccountNo(String chequeAccountNo) {
+		this.chequeAccountNo = chequeAccountNo;
+	}
+	public String getChequeHolderName() {
+		return chequeHolderName;
+	}
+	public void setChequeHolderName(String chequeHolderName) {
+		this.chequeHolderName = chequeHolderName;
+	}
+	public String getChequeBankName() {
+		return chequeBankName;
+	}
+	public void setChequeBankName(String chequeBankName) {
+		this.chequeBankName = chequeBankName;
+	}
+	public String getChequeIFSC() {
+		return chequeIFSC;
+	}
+	public void setChequeIFSC(String chequeIFSC) {
+		this.chequeIFSC = chequeIFSC;
+	}
+	public Date getChequeIssueDate() {
+		return chequeIssueDate;
+	}
+	public void setChequeIssueDate(Date chequeIssueDate) {
+		this.chequeIssueDate = chequeIssueDate;
+	}
+	public String getChequeStatus() {
+		return chequeStatus;
+	}
+	public void setChequeStatus(String chequeStatus) {
+		this.chequeStatus = chequeStatus;
+	}
+	
+	
+	
+}
