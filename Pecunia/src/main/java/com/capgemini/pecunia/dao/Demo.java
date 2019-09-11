72f4f807602555cc1e843e72f3f160a6c82bcf42
Anwesha98 <55138807+Anwesha98@users.noreply.github.com>
2019-09-11 12:31:00 +0530

    Anwesha

--- Pecunia/src/main/java/com/capgemini/pecunia/dao/Demo.java
+++ Pecunia/src/main/java/com/capgemini/pecunia/dao/Demo.java
@@ -2,7 +2,7 @@ package com.capgemini.pecunia.dao;
 
 public class Demo {
 	public static void main(String[] args) {
-		System.out.println("This is Anwesha");
+		System.out.println("Anwesha");
 		System.out.println("Hello");
 	}
 
