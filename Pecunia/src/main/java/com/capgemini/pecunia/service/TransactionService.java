14f64519a261a634a5e1d30e31c9967ffe600bf0
Rohan Patil <rohanpatil365@gmail.com>
2019-09-11 15:25:09 +0530

    trying something

---
+++ Pecunia/src/main/java/com/capgemini/pecunia/service/TransactionService.java
@@ -0,0 +1,6 @@
+package com.capgemini.pecunia.service;
+
+public interface TransactionService {
+	public void creditAccountUsingSlip(String accountNo,Double amount);
+//	public void creditAccountUsingCheque(String accountNo,Double)
+}
