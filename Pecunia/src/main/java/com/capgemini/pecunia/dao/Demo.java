f51aafd3e89552ac56c595a9f9208d309fa26c80
Rohan Patil <rohanpatil365@gmail.com>
2019-09-11 12:15:27 +0530

    Hi rohan

--- Pecunia/src/main/java/com/capgemini/pecunia/dao/Demo.java
+++ Pecunia/src/main/java/com/capgemini/pecunia/dao/Demo.java
@@ -3,6 +3,6 @@ package com.capgemini.pecunia.dao;
 public class Demo {
 	public static void main(String[] args) {
 		System.out.println("Hello");
-		System.out.println("Hi");
+		System.out.println("Hi from Rohan");
 	}
 }
