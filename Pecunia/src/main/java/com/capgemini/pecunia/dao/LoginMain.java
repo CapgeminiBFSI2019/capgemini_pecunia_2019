6fb49045f765bf9b1cb31b3ba224a992e82648be
saurabh5881 <55138472+saurabh5881@users.noreply.github.com>
2019-09-12 10:17:42 +0530

    added getters and setters

--- Pecunia/src/main/java/com/capgemini/pecunia/dao/LoginMain.java
+++ Pecunia/src/main/java/com/capgemini/pecunia/dao/LoginMain.java
@@ -5,6 +5,22 @@ public class LoginMain {
 	private String username;
 	private String password;
 	
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
 	public LoginMain() {
 		
 	}
