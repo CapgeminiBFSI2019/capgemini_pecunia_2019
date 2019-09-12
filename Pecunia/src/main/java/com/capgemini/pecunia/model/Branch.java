3791b62d9bc3fe1039666efef0ad236c507c2d23
Arpan1997 <arpanmondal1902@gmail.com>
2019-09-12 11:37:42 +0530

    hash code

--- Pecunia/src/main/java/com/capgemini/pecunia/model/Branch.java
+++ Pecunia/src/main/java/com/capgemini/pecunia/model/Branch.java
@@ -2,6 +2,53 @@ package com.capgemini.pecunia.model;
 
 public class Branch 
 {
+	@Override
+	public int hashCode() 
+	{
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((branchAddress == null) ? 0 : branchAddress.hashCode());
+		result = prime * result + ((branchIFSC == null) ? 0 : branchIFSC.hashCode());
+		result = prime * result + ((branchId == null) ? 0 : branchId.hashCode());
+		result = prime * result + ((branchName == null) ? 0 : branchName.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) 
+	{
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Branch other = (Branch) obj;
+		if (branchAddress == null) 
+		{
+			if (other.branchAddress != null)
+				return false;
+		} else if (!branchAddress.equals(other.branchAddress))
+			return false;
+		if (branchIFSC == null) 
+		{
+			if (other.branchIFSC != null)
+				return false;
+		} else if (!branchIFSC.equals(other.branchIFSC))
+			return false;
+		if (branchId == null) 
+		{
+			if (other.branchId != null)
+				return false;
+		} else if (!branchId.equals(other.branchId))
+			return false;
+		if (branchName == null) 
+		{
+			if (other.branchName != null)
+				return false;
+		} else if (!branchName.equals(other.branchName))
+			return false;
+		return true;
+	}
 	private String branchId;
 	private String branchName;
 	public Branch(String branchId, String branchName, String branchIFSC, String branchAddress) 
@@ -48,4 +95,3 @@ public class Branch
 	}
 
 }
-//hbhvcb
