a5cfb211018a4f1197856a1e3e950a62863e0213
Rohan Patil <rohanpatil365@gmail.com>
2019-09-12 16:24:37 +0530

    pushing

---
+++ Pecunia/src/main/java/com/capgemini/pecunia/service/TransactionService.java
@@ -0,0 +1,5 @@
+package com.capgemini.pecunia.service;
+
+public interface TransactionService {
+	public int creditUsingSlip();
+}
---
+++ Pecunia/src/main/java/com/capgemini/pecunia/service/TransactionServiceImpl.java
@@ -0,0 +1,11 @@
+package com.capgemini.pecunia.service;
+
+public class TransactionServiceImpl implements TransactionService{
+
+	@Override
+	public int creditUsingSlip() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
