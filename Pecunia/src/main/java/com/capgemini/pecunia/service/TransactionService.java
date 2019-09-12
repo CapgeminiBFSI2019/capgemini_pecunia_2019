e40cdc235c03d8ceadd87bee424b181982384aee
Rohan Patil <rohanpatil365@gmail.com>
2019-09-12 16:53:37 +0530

    Changed TransactionService and impl

--- Pecunia/src/main/java/com/capgemini/pecunia/service/TransactionService.java
+++ Pecunia/src/main/java/com/capgemini/pecunia/service/TransactionService.java
@@ -1,5 +1,10 @@
 package com.capgemini.pecunia.service;
 
+import java.util.Date;
+
 public interface TransactionService {
-	public int creditUsingSlip();
+	public int creditUsingSlip(String accountId,Double amount,Date transactionDate);
+	public int creditUsingCheque(String accountId,Double amount,Date transactionDate,String checkNum,String chequeAccount);
+	public int debitUsingSlip(String accountId,Double amount,Date transactionDate);
+	public int debitUsingCheque(String accountId,Double amount,Date transactionDate,String checkNum,String chequeAccount);
 }
--- Pecunia/src/main/java/com/capgemini/pecunia/service/TransactionServiceImpl.java
+++ Pecunia/src/main/java/com/capgemini/pecunia/service/TransactionServiceImpl.java
@@ -1,9 +1,31 @@
 package com.capgemini.pecunia.service;
 
+import java.util.Date;
+
 public class TransactionServiceImpl implements TransactionService{
 
 	@Override
-	public int creditUsingSlip() {
+	public int creditUsingSlip(String accountId, Double amount, Date transactionDate) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int creditUsingCheque(String accountId, Double amount, Date transactionDate, String checkNum,
+			String chequeAccount) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int debitUsingSlip(String accountId, Double amount, Date transactionDate) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int debitUsingCheque(String accountId, Double amount, Date transactionDate, String checkNum,
+			String chequeAccount) {
 		// TODO Auto-generated method stub
 		return 0;
 	}
