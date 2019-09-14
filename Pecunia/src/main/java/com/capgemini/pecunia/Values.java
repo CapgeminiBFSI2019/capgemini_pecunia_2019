5aa55f35b96f613ab69ab5033e9534e3afbff523
Rohan Patil <rohanpatil365@gmail.com>
2019-09-14 14:52:07 +0530

    Create Values.java

---
+++ Pecunia/src/main/java/com/capgemini/pecunia/Values.java
@@ -0,0 +1,8 @@
+package com.capgemini.pecunia;
+
+public class Values {
+	public static final String BANK_NAME = "PECUNIA";
+	public static final String TRANSACTION_DEBIT = "DEBIT";
+	public static final String TRANSACTION_CREDIT = "CREDIT";
+	public static final String[] OTHER_BANK_NAME = {"ICICI","SBI","HDFC","KOTAK","AXIS"};
+}
