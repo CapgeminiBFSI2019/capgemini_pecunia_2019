acab9547c0e56980f9ac9f7e422ebce5ae86e519
saurabh5881 <55138472+saurabh5881@users.noreply.github.com>
2019-09-17 16:23:13 +0530

    The Test Cases of Login

---
+++ Pecunia/src/test/java/com/capgemini/pecunia/dao/EmployeeDAOTest.java
@@ -0,0 +1,68 @@
+package com.capgemini.pecunia.dao;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class EmployeeDAOTest {
+
+	
+
+	@Test
+	public void loginUernameNull_Test() {
+		
+	String username = null;
+	assertNull(new EmployeeDAOimpl().login(username, "12345"));
+	
+	}
+
+	@Test
+	public void loginUsernameEmpty_Test() {
+		
+	String username = "";
+	assertNull(new EmployeeDAOimpl().login(username, "12345"));
+	
+	}
+	
+	@Test
+	public void loginPasswordNull_Test() {
+		
+	String passwString = null;
+	assertNull(new EmployeeDAOimpl().login("rohan", passwString));
+	
+	}
+	
+	@Test
+	public void loginPasswordEmpty_Test() {
+		
+	String passString = "";
+	assertNull(new EmployeeDAOimpl().login("avishek", passString));
+	
+	}
+	
+	
+	@Test
+	public void loginAllEmpty_Test() {
+		
+	assertNull(new EmployeeDAOimpl().login("", ""));
+	
+	}
+	
+	@Test
+	public void loginAllNull_Test() {
+		
+	assertNull(new EmployeeDAOimpl().login(null, null));
+	
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
